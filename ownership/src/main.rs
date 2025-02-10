1168b3c86c0c5c7b96b2a7175b46e4fc229729ce
Okafor Patrick C <patrickpeters911@gmail.com>
2025-02-10 07:03:18 +0100

    slices, ownership and borrwing

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -18,17 +18,60 @@ fn main() {
 
 
     let f = String::from("Hello");
-        let r1 = &f;
-        let r2 = &f;
+    let r1 = &f;
+    let r2 = &f;
 
-        println!("{r1} {r2}");
+    println!("{r1} {r2}");
+    let mut r3  = String::from("Joy is coming");
+    let c1 = &r3;
+    let c2 = &r3;
+    println!("{c1} and {c2}");
+    let c3 = &mut r3;
+    print!("{c3}\n");
 
-        let mut r3  = String::from("Joy is coming");
-        let c1 = &r3;
-        let c2 = &r3;
-        println!("{c1} and {c2}");
-        let c3 = &mut r3;
-        print!("{c3}\n");
+    let mut s = String::from("Hello world!");
+    let word = first_words(&s);
+    
+    println!("Word: {word}");
+
+    s.clear();
+
+    assert!(s.is_empty());
+    assert_eq!(0, s.len());
+    assert_ne!(s.len(), 17);
+    assert_eq!(12, s.capacity());
+
+    let s = String::from("hello world");
+
+    let hello = &s[..5];
+    println!("{hello}");
+    let world = &s[1 + hello.len()..];
+    println!("{world}");
+
+    let v = &s[..s.len()];
+    println!("{v}");
+
+    let a = [1, 2, 3, 4, 5, 6];
+    let slice = &a[1..3];
+    print!("slice: {:#?}\n", slice);
+
+    assert_eq!(slice, [2, 3]);
+
+}
+
+
+fn first_words(s: & str)-> &str {
+    let bytes = s.as_bytes();
+    print!("Bytes: {:?}\n", bytes);
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return  &s[..i];
+        } else{
+            print!("Item: {:?}\n", item);
+        }
+    }
+    return  &s[..];
 
 }
 
